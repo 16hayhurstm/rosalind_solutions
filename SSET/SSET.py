0d8a47f66ea1ecb16753b6ff7afdc5f98a932751
Myles Patrick Hayhurst <16hayhurstm@gmial.com>
2025-11-18 21:49:21 +0000

    add comments

--- SSET/SSET.py
+++ SSET/SSET.py
@@ -1,8 +1,9 @@
 MOD = 1_000_000
+# TotaL number of subsets of an n-element set is 2^n
 
 n = 849
 
-answer = pow(2, n, MOD)      # 2**n % MOD, but done efficiently
+answer = pow(2, n, MOD)      
 print(answer)
 
    
